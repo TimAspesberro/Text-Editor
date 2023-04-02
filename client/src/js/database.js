d74f0322c60593ce9f8a42aa3eb4b7775c0855a2
Tim Aspesberro <taspes@gmail.com>
2023-04-02 17:15:45 -0400

    added logic to database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,27 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (id, value) => {
+  console.error('Update jateDb')
 
+  const contactDb = await openDB('jate', 1);
+  const tx = contactDb.transaction('jate', 'readwrite');
+  const storeObj = tx.objectStore('jate');
+  const req = storeObj.put({ id: id, value: value })
+  const res = await req;
+
+  console.log('data saved to the DB', res);
+}
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('Get data from jateDb');
 
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const storeObj = tx.objectStore('jate');
+  const req = storeObj.getAll()
+  const res = await req;
+
+  console.log('data saved to the DB', res);
+}
 initdb();
