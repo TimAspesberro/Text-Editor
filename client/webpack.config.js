42da9750fb1d8843b772f2fede7bc0f42ec7f023
Tim Aspesberro <taspes@gmail.com>
2023-04-12 23:43:42 -0400

    touched up package,json to fix application error

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -20,7 +20,6 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Text Editor'
       }),
       new InjectManifest({
         swSrc: './src-sw.js',
